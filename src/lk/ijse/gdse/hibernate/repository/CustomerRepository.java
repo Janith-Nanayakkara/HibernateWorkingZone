f7dda829e6974e6e86ade4c4a64f767550a2a973
Janith-Nanayakkara <janithnanayakkara0202@gmail.com>
2023-02-25 15:25:54 +0530

    CustomerRepository

---
+++ src/lk/ijse/gdse/hibernate/repository/CustomerRepository.java
@@ -0,0 +1,21 @@
+package lk.ijse.gdse.hibernate.repository;
+
+import lk.ijse.gdse.hibernate.entity.Customer;
+import lk.ijse.gdse.hibernate.util.SessionFactoryConfiguration;
+import org.hibernate.Session;
+import org.hibernate.Transaction;
+
+import java.io.Serializable;
+
+public class CustomerRepository {
+        private final Session session;
+    public CustomerRepository() {
+        session = SessionFactoryConfiguration.getInstance().getSession();
+    }
+    public Integer saveCustomer(Customer customer){
+        Transaction transaction = session.beginTransaction();
+        Integer id = (Integer) session.save(customer);
+        transaction.commit();
+        return id;
+    }
+}
