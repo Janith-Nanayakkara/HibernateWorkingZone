895dae37fa7a1f54456de6381e13192b14eac60c
Janith-Nanayakkara <janithnanayakkara0202@gmail.com>
2023-02-25 16:02:30 +0530

    getCustomer

--- src/lk/ijse/gdse/hibernate/repository/CustomerRepository.java
+++ src/lk/ijse/gdse/hibernate/repository/CustomerRepository.java
@@ -8,11 +8,13 @@ import org.hibernate.Transaction;
 import java.io.Serializable;
 
 public class CustomerRepository {
-        private final Session session;
+    private final Session session;
+
     public CustomerRepository() {
         session = SessionFactoryConfiguration.getInstance().getSession();
     }
-    public Integer saveCustomer(Customer customer){
+
+    public Integer saveCustomer(Customer customer) {
         Transaction transaction = session.beginTransaction();
 /*        Integer id = (Integer) session.save(customer);
         transaction.commit();*/
@@ -22,7 +24,7 @@ public class CustomerRepository {
             transaction.commit();
             session.close();
             return id;
-        }catch (Exception ex){
+        } catch (Exception ex) {
             transaction.rollback();
             session.close();
             ex.printStackTrace();
@@ -31,14 +33,14 @@ public class CustomerRepository {
 
     }
 
-    public boolean updateCustomer(Customer customer){
+    public boolean updateCustomer(Customer customer) {
         Transaction transaction = session.beginTransaction();
         try {
             session.update(customer);
             transaction.commit();
             session.close();
             return true;
-        }catch (Exception e){
+        } catch (Exception e) {
             transaction.rollback();
             session.close();
             e.printStackTrace();
@@ -46,4 +48,13 @@ public class CustomerRepository {
         }
     }
 
+    public Customer getCustomer(long id) {
+        try {
+            return session.get(Customer.class, id);
+
+        } catch (Exception e) {
+            e.printStackTrace();
+            throw e;
+        }
+    }
 }
