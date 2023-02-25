cea8f65557141d371a5638ea8a70de5692e17d03
Janith-Nanayakkara <janithnanayakkara0202@gmail.com>
2023-02-25 15:50:41 +0530

    repositary

--- src/lk/ijse/gdse/hibernate/AppInitializer.java
+++ src/lk/ijse/gdse/hibernate/AppInitializer.java
@@ -3,6 +3,7 @@ package lk.ijse.gdse.hibernate;
 import lk.ijse.gdse.hibernate.embaded.CustName;
 import lk.ijse.gdse.hibernate.embaded.MobileNumber;
 import lk.ijse.gdse.hibernate.entity.Customer;
+import lk.ijse.gdse.hibernate.repository.CustomerRepository;
 import lk.ijse.gdse.hibernate.util.SessionFactoryConfiguration;
 import org.hibernate.Session;
 import org.hibernate.Transaction;
@@ -12,11 +13,14 @@ import java.util.List;
 
 public class AppInitializer {
     public static void main(String[] args) {
-        Customer customer = new Customer();
+        CustomerRepository customerRepository = new CustomerRepository();
+       Customer customer = getCustomerEntity();
+       customerRepository.saveCustomer(customer);
+       /* Customer customer = new Customer();
         customer.setId(1);
-        /*customer.setName("janith");
+        *//*customer.setName("janith");
         customer.setAddress("Galle");
-        customer.setSalary(45000.00);*/
+        customer.setSalary(45000.00);*//*
 
         CustName custName= new CustName();
         custName.setFirstName("Janith");
@@ -35,6 +39,25 @@ public class AppInitializer {
         Transaction transaction = session.beginTransaction();
         session.save(customer);
         transaction.commit();
-        session.close();
+        session.close();*/
+    }
+        private static Customer getCustomerEntity() {
+            Customer customer = new Customer();
+            customer.setId(1);
+            CustName custName = new CustName();
+            custName.setFirstName("Janith");
+            custName.setMiddleName("Induwara");
+            custName.setFirstName("Nanayakkara");
+
+            customer.setName(custName);
+            customer.setAddress("Ahanagama");
+            customer.setSalary(50000.00);
+
+            List<MobileNumber> phonNumbers = new ArrayList<>();
+            phonNumbers.add(new MobileNumber("DILOGE", "0760904402"));
+            phonNumbers.add(new MobileNumber("MOBILET", "07123456790"));
+
+            return customer;
+
     }
 }
